e94c5e2c8df42f6b0fc4745eee33511a6310488a
Max Stevenson <maxstevenson@msn.com>
2019-07-29 17:46:55 +0100

    Challenge four completed

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,5 +1,39 @@
 const getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
+
 const msg = getNotes()
 
-console.log(chalk.bold.green('Success!'));
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing note...')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all stored notes',
+    handler: function() {
+        console.log('Here are your notes...')
+    }
+})
+
+yargs.command({
+    command: 'Read',
+    describe: 'Read a selected note',
+    handler: function() {
+        console.log('Reading note...')
+    }
+})
+
+console.log(yargs.argv);
