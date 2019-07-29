5c4c17c2a9647d3378f74973c3bc539044ef0118
Luke Griffith <lukemgriffith@gmail.com>
2019-07-29 10:04:31 +0100

    Update app.js

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 
 fs.writeFileSync('notes.txt', 'My name is Max\n');
 fs.appendFileSync('notes.txt', 'This challenge was pretty easy');
+fs.appendFileSync('notes.txt', 'git gud son');
