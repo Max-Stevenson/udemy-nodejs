6d8060f7f6f273c9182eb89ca05aff489569b746
Max Stevenson <maxstevenson@msn.com>
2019-07-30 20:28:08 +0100

    Fifth challenge completed - added remove note function in notes.js. - Modified app.js to call removeNote function. - Included resource notes.json file in gitignore.

--- NotesApp/notes.js
+++ NotesApp/notes.js
@@ -6,16 +6,48 @@ const getNotes = function () {
 
 const addNote = function (title, body) {
     const notes = loadNotes();
-    console.log(notes);
-    notes.push({
-        
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title;
     })
+    
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log('new note added...')
+    } else {
+        console.log('Note title: ' + title + ' is already taken!');
+    }
+}
+
+const removeNote = function(title) {
+    const notes = loadNotes();
+    var notesLength = notes.length;
+    for(var i = 0; i < notes.length; i++) {
+        if(notes[i].title === title) {
+            notes.splice(i, 1);
+        }
+    }
+    var newNotesLength = notes.length;
+    if(notesLength != newNotesLength) {
+        console.log('Note with title: ' + title + ' deleted' )
+    } else {
+        console.log('No note found with title: ' + title);
+    }
+    saveNotes(notes);
+}
+
+const saveNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
 }
 
 const loadNotes = function() {
     try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJSON = dataBuffer.toString;
+        const dataBuffer = fs.readFileSync("notes.json");
+        const dataJSON = dataBuffer.toString();
         return JSON.parse(dataJSON);
     } catch (e) {
         return [];
@@ -25,5 +57,6 @@ const loadNotes = function() {
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 };
