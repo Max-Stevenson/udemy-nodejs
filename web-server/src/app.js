6e623e4af79b7b1b041a4d336577c6748fe26d12
Max Stevenson <maxstevenson@msn.com>
2019-08-09 18:14:06 +0100

    Refactor to utilise handlebars - deleted static pages. - utilised templating with handlebars. - index, about and help pages created.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,28 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
+app.set('view engine', 'hbs');
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Max Stevenson'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'Hope this helps...'
+    });
+});
+
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Madison, WI',
