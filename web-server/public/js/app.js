8fe3fe1839ff57a4584a5f225652bc8a7c2c10cd
Max Stevenson <maxstevenson@msn.com>
2019-08-12 20:02:38 +0100

    Weather forecast rendered - forecast is now successfully fetched and returned to index. - error handling implemented. - basic styling implemented.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,18 +2,24 @@ console.log("Client side js file is loaded");
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const forecastParagraph = document.querySelector('#forecast');
+const errorParagraph = document.querySelector('#errorMessage');
+
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const location = search.value;
+    errorParagraph.textContent = '';
+    forecastParagraph.textContent= 'Loading...';
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                forecastParagraph.textContent = '';
+                errorParagraph.textContent = data.error;
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                forecastParagraph.textContent = data.location + ". " + data.forecast;
+                
             };
         });
     });
