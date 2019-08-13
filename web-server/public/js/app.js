3b59734b098c33c01b912cca1495da54abb57705
Max Stevenson <maxstevenson@msn.com>
2019-08-13 18:26:45 +0100

    Ready for deployment to Heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (event) => {
     errorParagraph.textContent = '';
     forecastParagraph.textContent= 'Loading...';
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 forecastParagraph.textContent = '';
