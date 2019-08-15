f52ac1ed2e6861b757434d7e492bea5859bf737f
Max Stevenson <maxstevenson@msn.com>
2019-08-15 14:03:46 +0100

    Task Manager setup completed

---
+++ task-manager/mongodb.js
@@ -0,0 +1,12 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('unable to connect to database');    
+    };
+    console.log('connected correctly');
+});
