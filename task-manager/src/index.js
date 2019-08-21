e82bed9708fc15b5aeed98253ab84c8763b07042
Max Stevenson <maxstevenson@msn.com>
2019-08-21 07:46:50 +0100

    User routes extracted to separate file

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,49 +2,19 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
+const userRouter = require('./routers/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-    try {
-        await user.save()
-        res.status(201).send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
+app.use(userRouter);
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
     try {
         await task.save()
         res.status(201).send(task);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-        };
-        res.status(200).send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).send(users);
     } catch (error) {
         res.status(500).send(error);
     };
@@ -72,28 +42,7 @@ app.get('/tasks', async (req, res) => {
     };
 });
 
-app.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    const body = req.body;
-    const allowedUpdate = ['name', 'email', 'password', 'age'];
-    const updates = Object.keys(body);
-    const isValidOperation = updates.every((item) => {return allowedUpdate.includes(item)});
 
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'You cannot update this field'});
-    };
-
-    try {
-        const user = await User.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
-
-        if (!user) {
-            return res.status(404).send();
-        };
-        res.status(200).send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
 
 app.patch('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
@@ -112,19 +61,6 @@ app.patch('/tasks/:id', async (req, res) => {
             return res.status(404).send();
         };
         res.status(200).send(task)
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
-
-app.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findByIdAndDelete(_id);
-        if (!user) {
-            return res.status(404).send();
-        };
-        res.status(200).send(user);
     } catch (error) {
         res.status(500).send(error);
     };
