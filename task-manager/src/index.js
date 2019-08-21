d6e3d931c95e1fe805f9e1618612db10e1c4d24d
Max Stevenson <maxstevenson@msn.com>
2019-08-21 20:44:35 +0100

    Task routes extracted to separate file

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,81 +3,14 @@ require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
 const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-    try {
-        await task.save()
-        res.status(201).send(task);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const task = await Task.findById(_id);
-        if (!task) {
-            return res.status(404).send();
-        };
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send();
-    };
-});
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.status(200).send(tasks);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
-
-
-
-app.patch('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
-    const body = req.body;
-    const allowedUpdate = ['compleated', 'description'];
-    const updates = Object.keys(body);
-    const isValidOperation = updates.every((item) => {return allowedUpdate.includes(item)});
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: "You cannot update this field"});
-    };
-
-    try {
-        const task = await Task.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
-        if (!task) {
-            return res.status(404).send();
-        };
-        res.status(200).send(task)
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const task = await Task.findByIdAndDelete(_id);
-        if (!task) {
-            return res.status(404).send();
-        };
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error);
-    };
-});
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port: ' + port);
---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const Task = require('../models/task');
+const router = new express.Router();
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save()
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    };
+});
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        };
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send();
+    };
+});
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    };
+});
+
+router.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+    const allowedUpdate = ['compleated', 'description'];
+    const updates = Object.keys(body);
+    const isValidOperation = updates.every((item) => {return allowedUpdate.includes(item)});
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: "You cannot update this field"});
+    };
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
+        if (!task) {
+            return res.status(404).send();
+        };
+        res.status(200).send(task)
+    } catch (error) {
+        res.status(500).send(error);
+    };
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findByIdAndDelete(_id);
+        if (!task) {
+            return res.status(404).send();
+        };
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    };
+});
+
+module.exports = router;
