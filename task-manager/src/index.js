a6ca86327c3fbd2124e050947ea923d0d983705d
Max Stevenson <maxstevenson@msn.com>
2019-08-21 21:39:04 +0100

    Refactor to User route and User model - user model now utilises schema - user route now ensures update route triggers middleware - password hashed before saving to db

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -13,18 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port: ' + port);
 });
-
-const bcrypt = require('bcrypt');
-
-const myFunction = async () => {
-    const password = 'Red12345';
-    const hashedPassword = await bcrypt.hash(password, 8);
-
-    console.log(password);
-    console.log(hashedPassword);
-
-    const isMatch = await bcrypt.compare(password, hashedPassword);
-    console.log(isMatch);
-};
-
-myFunction().then().catch();
