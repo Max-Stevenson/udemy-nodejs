5d92fd414fc3963b323dbd86526597e00b614f53
Max Stevenson <maxstevenson@msn.com>
2019-08-24 10:26:53 +0100

    Added route to clear all user tokens

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -37,7 +37,18 @@ router.post('/users/logout', auth, async (req, res) => {
         res.status(200).send();
     } catch (error) {
         res.status(500).send();
-    }
+    };
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.status(200).send();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    };
 });
 
 router.get('/users/me', auth, async (req, res) => {
