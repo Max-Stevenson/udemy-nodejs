fe7b6e108a440b02f484e0f2de873fae28cae21e
Max Stevenson <maxstevenson@msn.com>
2019-08-29 22:10:12 +0100

    Sharp installed - modified avatar upload route. - images now saved as .png. - images now resized to 250 x 250.

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/authentication');
 const router = new express.Router();
@@ -66,7 +67,8 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.status(200).send();
 }, (error, req, res, next) => {
@@ -87,7 +89,7 @@ router.get('/users/:id/avatar', async (req, res) => {
         if(!user || !user.avatar){
             throw new Error();
         }
-        res.set('Content-Type', 'image/jpg');
+        res.set('Content-Type', 'image/png');
         res.send(user.avatar);
     } catch (error) {        
         res.status(404).send()
