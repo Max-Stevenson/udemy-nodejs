1d7833af2ee9a9eca33764d77c0012476e7c1319
Max Stevenson <maxstevenson@msn.com>
2019-09-06 19:26:46 +0100

    More experimentation with jest

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math');
+const { calculateTip, add } = require('../src/math');
 
 test('should caculate total with tip', () => {
     const total = calculateTip(10, .3);
@@ -8,4 +8,23 @@ test('should caculate total with tip', () => {
 test('should calculate total with default tip', () => {
     const total = calculateTip(10);
     expect(total).toBe(11);
+});
+
+test('async test demo', (done) => {
+    setTimeout(() => {
+        expect(1).toBe(1);
+        done();
+    }, 2000);
+});
+
+test('another async test', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5);
+    });
+    done();
+});
+
+test('yet another async example', async() => {
+    const sum = await add(2, 3);
+    expect(sum).toBe(5);
 });
