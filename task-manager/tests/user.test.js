e1edfc0f107ba288f98cfd0ad51c289c771af8eb
Max Stevenson <maxstevenson@msn.com>
2019-09-09 20:26:58 +0100

    Before hook added to user test route to delete db data

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,10 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+beforeEach(async() => {
+    await User.deleteMany();
+});
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
