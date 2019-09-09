6d67420f5732b90577d4fd8e02dd2693e36059da
Max Stevenson <maxstevenson@msn.com>
2019-09-09 20:36:43 +0100

    Tests developed for User - test for correct login - test for incorrect login

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -2,8 +2,15 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const userOne = {
+    name: 'testUser',
+    email: 'testUser@testing.com',
+    password: 'testing101'
+}
+
 beforeEach(async() => {
     await User.deleteMany();
+    await new User(userOne).save();
 });
 
 test('Should sign up a new user', async () => {
@@ -12,4 +19,18 @@ test('Should sign up a new user', async () => {
         email: 'jestTest@test.com',
         password: 'jestTesting101'
     }).expect(201);
+});
+
+test('Should log in existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);
+});
+
+test('Should not log in nonexistent user', async() => {
+    await request(app).post('/users/login').send({
+        email: 'nonexistent@nonexistent.com',
+        password: 'doesnotexist'
+    }).expect(400);
 });
