ea2922c991e8d03038520d77ff92eb63cc906f83
Max Stevenson <maxstevenson@msn.com>
2019-09-09 20:47:44 +0100

    Tests added to GET of user profile - added authentication token to sample user

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,12 +1,21 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const userOneId = new mongoose.Types.ObjectId();
+
+
 const userOne = {
+    _id: userOneId,
     name: 'testUser',
     email: 'testUser@testing.com',
-    password: 'testing101'
-}
+    password: 'testing101',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
+};
 
 beforeEach(async() => {
     await User.deleteMany();
@@ -34,3 +43,16 @@ test('Should not log in nonexistent user', async() => {
         password: 'doesnotexist'
     }).expect(400);
 });
+
+test('Should GET user profile', async() => {
+    await request(app).get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('Should not GET user profile for unauthenticated user', async() => {
+    await request(app).get('/users/me')
+    .send()
+    .expect(401);
+})
