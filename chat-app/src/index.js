26751c371db94bfff55341cad1c2042540c6960d
Max Stevenson <maxstevenson@msn.com>
2019-10-17 19:26:26 +0100

    SocketIO installed - setup socketio in index.js

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 const path = require('path');
 const router = express.Router();
 const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+// setup for server to support web sockets
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -10,7 +17,11 @@ router.get('/', (req,res) => {
     res.sendFile(publicDirectoryPath + '/index.html');
 });
 
+io.on('connection', () => {
+    console.log('new websocket connection');
+});
+
 app.use('/', router);
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`chat app listening on port ${port}!`)
 });
