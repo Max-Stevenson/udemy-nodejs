23aaaa63a08a57c89597491193b472117a7f74d5
Max Stevenson <maxstevenson@msn.com>
2019-10-17 20:25:34 +0100

    Learning about socket.emit

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,11 @@
-io();
+const socket = io();
+
+socket.on('countUpdated', (count) => {
+    console.log('count updated ' + count); 
+});
+
+const buttonOne = document.querySelector('#buttonOne');
+
+buttonOne.addEventListener('click', () => {
+    socket.emit('incremented');
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,11 +11,20 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+
 const server = app.listen(port, () => {
     console.log(`chat app listening on port ${port}!`)
 });
 
 const io = socketio(server);
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('new websocket connection');
+    socket.emit('countUpdated', count);
+
+    socket.on('incremented', () => {
+        count++;
+        io.emit('countUpdated', count)
+        // emits event to all connections
+    });
 });
