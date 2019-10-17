b8a9abb3eeea5252c0f9b2be6f3bf37af7b81994
Max Stevenson <maxstevenson@msn.com>
2019-10-17 19:52:35 +0100

    Added client side support for socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,25 +3,19 @@ const http = require('http');
 const socketio = require('socket.io');
 
 const path = require('path');
-const router = express.Router();
 const app = express();
-const server = http.createServer(app);
-
-const io = socketio(server);
 // setup for server to support web sockets
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-router.get('/', (req,res) => {
-    res.sendFile(publicDirectoryPath + '/index.html');
+app.use(express.static(publicDirectoryPath));
+
+const server = app.listen(port, () => {
+    console.log(`chat app listening on port ${port}!`)
 });
 
+const io = socketio(server);
 io.on('connection', () => {
     console.log('new websocket connection');
-});
-
-app.use('/', router);
-server.listen(port, () => {
-    console.log(`chat app listening on port ${port}!`)
 });
