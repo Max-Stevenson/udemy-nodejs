cceef1fc382c7b4dd542ae82029137c62e1ea13a
Max Stevenson <maxstevenson@msn.com>
2019-10-18 10:35:24 +0100

    Chat functionality implemented

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,11 +1,28 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('count updated ' + count); 
+// socket.on('countUpdated', (count) => {
+//     console.log('count updated ' + count); 
+// });
+
+// const buttonOne = document.querySelector('#buttonOne');
+
+// buttonOne.addEventListener('click', () => {
+//     socket.emit('incremented');
+// });
+
+socket.on('welcomeMessage', (eventData) => {
+    console.log(eventData); 
 });
 
-const buttonOne = document.querySelector('#buttonOne');
+const chatForm = document.getElementsByClassName('chatForm')[0];
+const messageInput = document.getElementById('messageBox');
 
-buttonOne.addEventListener('click', () => {
-    socket.emit('incremented');
+chatForm.addEventListener('submit', () => {
+    event.preventDefault();
+    const message = messageInput.value;
+    socket.emit('clientMessage', message);
+});
+
+socket.on('showMessage', (message) => {
+    console.log(message);  
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,7 +11,8 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+// let count = 0;
+let eventData = 'welcome to server!'
 
 const server = app.listen(port, () => {
     console.log(`chat app listening on port ${port}!`)
@@ -20,11 +21,15 @@ const server = app.listen(port, () => {
 const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('new websocket connection');
-    socket.emit('countUpdated', count);
+    socket.emit('welcomeMessage', eventData);
+    // socket.emit('countUpdated', count);
 
-    socket.on('incremented', () => {
-        count++;
-        io.emit('countUpdated', count)
-        // emits event to all connections
+    // socket.on('incremented', () => {
+    //     count++;
+    //     io.emit('countUpdated', count)
+    //     // emits event to all connections
+    // });
+    socket.on('clientMessage', (message) => {
+        io.emit('showMessage', message);
     });
 });
